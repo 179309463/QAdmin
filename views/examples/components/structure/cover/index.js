d0cd073df32dfee2e808c32b27a78e188aa3201b
fengzhuojia <179309463@qq.com>
2019-04-10 09:03:55 +0800

    plyr upgrade

---
+++ views/examples/components/structure/cover/index.js
@@ -0,0 +1,16 @@
+/**
+ * QAdmin v1.2.0 (http://www.qadmin.com/)
+ * Copyright 2015-2017 QAdmin Team
+ * Licensed under the QAdmin License 1.0 (http://www.qadmin.com/about/#license)
+ */
+(function (document, window) {
+    'use strict';
+
+    window.Content = App.extend({
+        run: function(next){
+            new Plyr("video");
+            new Plyr("audio");
+            next();
+        }
+    });
+})(document, window);
